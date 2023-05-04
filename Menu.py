f3d09377dc1ff4421d9294805fd72495ce9a9475
Chani Viner <czilbers@g.jct.ac.il>
2023-05-04 13:52:23 +0300

    Initial commit

---
+++ Menu.py
@@ -0,0 +1,15 @@
+def 
+
+
+while True:
+    user_action = input("Please select one of the options")
+    match user_action:
+        case '1':
+            print("1")
+        case '2':
+            print("2")
+        case '3':
+            break
+        case _:
+            print("other")
+
