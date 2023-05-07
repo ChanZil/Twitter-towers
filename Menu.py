05cdaf5e4445aed41decc622fee6b915d2a409f7
Chani Viner <czilbers@g.jct.ac.il>
2023-05-07 13:09:13 +0300

    2

--- Menu.py
+++ Menu.py
@@ -3,16 +3,20 @@ import functions
 
 def main():
     while True:
-        user_action = input("Please select one of the options")
+        user_action = input("Press 1 for rectangle tower, 2 for triangle tower or 3 to exit: ")
         match user_action:
             case '1':
-                print("1")
+                width = input("Insert the width of the rectangle tower: ")
+                height = input("Insert the height of the rectangle tower: ")
+                functions.rectangle(int(width), int(height))
             case '2':
-                print("2")
+                width = input("Insert the width of the triangle tower: ")
+                height = input("Insert the height of the triangle tower: ")
+                functions.triangle(int(width), int(height))
             case '3':
                 break
             case _:
-                print("other")
+                print("Invalid choice")
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,34 +1,78 @@
 import math
 
 
-def rectangle(length, height):
-    if length == height or abs(length - height) > 5:
-        rectangle_area = length * height
+def rectangle(width, height):
+    """
+    prints the scope or the area of a rectangle
+    :param width: the width of the rectangle
+    :param height: the height of the rectangle
+    :return: none
+    """
+    if width == height or abs(width - height) > 5:
+        rectangle_area = str(width * height)
         print("The area of the rectangle is: " + rectangle_area)
     else:
-        rectangle_scope = length * 2 + height * 2
+        rectangle_scope = str(width * 2 + height * 2)
         print("The scope of the rectangle is: " + rectangle_scope)
 
 
-def triangle(length, height):
-    triangle_option = input()
+def triangle(width, height):
+    """
+    Manages the triangle options.
+    :param width: the width of the triangle
+    :param height: the height of the triangle
+    :return: none
+    """
+    triangle_option = input("Press 1 for triangle scope or 2 for printing the triangle: ")
     match triangle_option:
         case '1':
-            triangle_scope(length, height)
+            triangle_scope(width, height)
         case '2':
-            if length % 2 == 0 or length > height * 2:
-                print("2")
+            if width % 2 == 0 or width > height * 2:
+                print("It is not possible to print the triangle")
             else:
-                print_triangle(length, height)
+                print_triangle(width, height)
         case _:
-            print("invalid choice")
+            print("Invalid choice")
 
 
-def triangle_scope(length, height):
-    side = math.sqrt(height ** 2 + (length / 2) ** 2)
-    scope = side * 2 + length
+def triangle_scope(width, height):
+    """
+    Prints the scope of the triangle
+    :param width:
+    :param height:
+    :return: none
+    """
+    side = math.sqrt(height ** 2 + (width / 2) ** 2)
+    scope = str(side * 2 + width)
     print("The scope of the triangle is: " + scope)
 
 
-def print_triangle(length, height):
-    print("triangle")
+def print_triangle(width, height):
+    """
+    Prints the triangle shape
+    :param width:
+    :param height:
+    :return: none
+    """
+    i_height = 1
+    i_width = 1
+    lines_amount = 0
+    top_lines_amount = 0
+    if height > 2:
+        lines_amount = int((height - 2) / int((width - 2) / 2))
+        top_lines_amount = (height - 2) % int((width - 2) / 2) + lines_amount
+    while i_height <= height:
+        star = '*' * i_width
+        num_spaces = int((width - i_width) / 2)
+        space = ' ' * num_spaces
+        if i_width == 1 or i_width == width:
+            it = 1
+        elif i_width == 3:
+            it = top_lines_amount
+        else:
+            it = lines_amount
+        for i in range(it):
+            print(space + star + space)
+            i_height += 1
+        i_width += 2
